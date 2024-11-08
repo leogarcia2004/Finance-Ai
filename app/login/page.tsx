a37465c8b4c452e5ca1babc45f56233a51020205
Leonardo Garcia <leonardogarcia1914@hotmail.com>
2024-11-08 00:50:07 -0300

    feat: add authentication with clerk

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,9 +1,21 @@
 import Image from "next/image"
 import { Button } from "../_components/ui/button"
 import { LogInIcon } from "lucide-react"
+import { SignInButton } from "@clerk/nextjs"
+import { auth } from "@clerk/nextjs/server"
+import { redirect } from "next/navigation"
 
 
-const LoginPage = () => {
+const LoginPage = async () => { // Um componente pode ser assíncrono.
+
+  // Lógica para quando o usuário estiver logado, ele não ver a página de login.
+  const { userId } = await auth() // o auth() vai retornar o userId se o usuário estiver logado. Ele vai pegar o osuário que estiver logado. 
+  // userId é o usuário logado.
+  if(userId) { // se userId existir, ou seja, se o usuário estiver logado.
+    redirect("/") // Redireciona para a página inicial.
+  }
+
+
   return (
     <div className="grid grid-cols-2 h-full">
 
@@ -11,13 +23,16 @@ const LoginPage = () => {
           <Image src="/logo.svg" width={173} height={39} alt="Finance AI" className="mb-8"/>
           <h1 className="text-4xl font-bold mb-3">Bem-vindo</h1>
           <p className="text-muted-foreground mb-8">A Finance AI é uma plataforma de gestão financeira que utiliza IA para monitorar suas movimentações, e oferecer insights personalizados, facilitando o controle do seu orçamento.</p>
-          <Button  variant="outline">
-            <LogInIcon  className="mr-2"/>
-            Fazer login ou criar conta
-          </Button>
+          <SignInButton >
+            <Button  variant="outline">
+              <LogInIcon  className="mr-2"/>
+              Fazer login ou criar conta
+            </Button>
+          </SignInButton>
+          
         </div>
         <div className="relative h-full w-full">
-          <Image src="/Login.png" alt="Faça login" fill className="object-cover"/> {/* Quando coloco 'fill', significa que ela vai ocupar toda a div. */}
+          <Image src="/Login.png" alt="Faça login" fill className="object-cover"/>
         </div>       
     </div>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,20 @@
-import { Button } from "./_components/ui/button";
+import { UserButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { dark } from "@clerk/themes";
+import { redirect } from "next/navigation";
 
+ const Home = async () => {
+  const { userId } = await auth()
+  if(!userId) {
+    redirect("/login")
+  }
 
-export default function Home() {
   return (
-      <Button>Click me</Button>
+      <div className="h-full justify-center flex items-center">
+          <UserButton showName />
+          
+      </div>
   );
 }
+
+export default Home;
