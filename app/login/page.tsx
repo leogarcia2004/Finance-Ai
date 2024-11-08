60b28bb9af823816efbb75b11c44cb2230b35828
Leonardo Garcia <leonardogarcia1914@hotmail.com>
2024-11-08 01:47:35 -0300

    chore: add git hooks setup

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,41 +1,54 @@
-import Image from "next/image"
-import { Button } from "../_components/ui/button"
-import { LogInIcon } from "lucide-react"
-import { SignInButton } from "@clerk/nextjs"
-import { auth } from "@clerk/nextjs/server"
-import { redirect } from "next/navigation"
+import Image from "next/image";
+import { Button } from "../_components/ui/button";
+import { LogInIcon } from "lucide-react";
+import { SignInButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-
-const LoginPage = async () => { // Um componente pode ser assíncrono.
+const LoginPage = async () => {
+  // Um componente pode ser assíncrono.
 
   // Lógica para quando o usuário estiver logado, ele não ver a página de login.
-  const { userId } = await auth() // o auth() vai retornar o userId se o usuário estiver logado. Ele vai pegar o osuário que estiver logado. 
+  const { userId } = await auth(); // o auth() vai retornar o userId se o usuário estiver logado. Ele vai pegar o osuário que estiver logado.
   // userId é o usuário logado.
-  if(userId) { // se userId existir, ou seja, se o usuário estiver logado.
-    redirect("/") // Redireciona para a página inicial.
+  if (userId) {
+    // se userId existir, ou seja, se o usuário estiver logado.
+    redirect("/"); // Redireciona para a página inicial.
   }
 
-
   return (
-    <div className="grid grid-cols-2 h-full">
-
-        <div className=" mx-auto flex flex-col h-full max-w-[550px] justify-center p-8">
-          <Image src="/logo.svg" width={173} height={39} alt="Finance AI" className="mb-8"/>
-          <h1 className="text-4xl font-bold mb-3">Bem-vindo</h1>
-          <p className="text-muted-foreground mb-8">A Finance AI é uma plataforma de gestão financeira que utiliza IA para monitorar suas movimentações, e oferecer insights personalizados, facilitando o controle do seu orçamento.</p>
-          <SignInButton >
-            <Button  variant="outline">
-              <LogInIcon  className="mr-2"/>
-              Fazer login ou criar conta
-            </Button>
-          </SignInButton>
-          
-        </div>
-        <div className="relative h-full w-full">
-          <Image src="/Login.png" alt="Faça login" fill className="object-cover"/>
-        </div>       
+    <div className="grid h-full grid-cols-2">
+      <div className="mx-auto flex h-full max-w-[550px] flex-col justify-center p-8">
+        <Image
+          src="/logo.svg"
+          width={173}
+          height={39}
+          alt="Finance AI"
+          className="mb-8"
+        />
+        <h1 className="mb-3 text-4xl font-bold">Bem-vindo</h1>
+        <p className="mb-8 text-muted-foreground">
+          A Finance AI é uma plataforma de gestão financeira que utiliza IA para
+          monitorar suas movimentações, e oferecer insights personalizados,
+          facilitando o controle do seu orçamento.
+        </p>
+        <SignInButton>
+          <Button variant="outline">
+            <LogInIcon className="mr-2" />
+            Fazer login ou criar conta
+          </Button>
+        </SignInButton>
+      </div>
+      <div className="relative h-full w-full">
+        <Image
+          src="/Login.png"
+          alt="Faça login"
+          fill
+          className="object-cover"
+        />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default LoginPage
+export default LoginPage;
--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,18 @@
 import { UserButton } from "@clerk/nextjs";
 import { auth } from "@clerk/nextjs/server";
-import { dark } from "@clerk/themes";
 import { redirect } from "next/navigation";
 
- const Home = async () => {
-  const { userId } = await auth()
-  if(!userId) {
-    redirect("/login")
+const Home = async () => {
+  const { userId } = await auth();
+  if (!userId) {
+    redirect("/login");
   }
 
   return (
-      <div className="h-full justify-center flex items-center">
-          <UserButton showName />
-          
-      </div>
+    <div className="flex h-full items-center justify-center">
+      <UserButton showName />
+    </div>
   );
-}
+};
 
 export default Home;
